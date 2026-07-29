e982a2f4417f2f05523f9e2a8ef7de4647ad7dfd
mo72med <52377349+mo72med@users.noreply.github.com>
2026-07-29 19:16:30 +0300

    Add result type

---
+++ packages/core/lib/src/result.dart
@@ -0,0 +1,18 @@
+sealed class Result<T> {
+  const Result();
+
+  const factory Result.success(T value) = Success<T>;
+  const factory Result.failure(Failure failure) = FailureResult<T>;
+}
+
+final class Success<T> extends Result<T> {
+  const Success(this.value);
+
+  final T value;
+}
+
+final class FailureResult<T> extends Result<T> {
+  const FailureResult(this.failure);
+
+  final Failure failure;
+}
