3a507ca97f0f50dc7a18f1392c31d678078112a0
mo72med <52377349+mo72med@users.noreply.github.com>
2026-07-30 00:20:34 +0300

    Add reflection repository abstraction

---
+++ packages/reflection/lib/src/reflection_repository.dart
@@ -0,0 +1,7 @@
+import 'package:tazkiyah_core/core.dart';
+
+import 'reflection.dart';
+
+abstract class ReflectionRepository extends Repository<Reflection, Id> {
+  const ReflectionRepository();
+}
